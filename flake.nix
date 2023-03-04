54b652a773e903e7088ebe86b0052d83ddea28ec
Philipp Kern <phil@philkern.de>
2023-03-04 20:20:31 +0100

    Add a Nix flake.

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Basic monitoring rules.";
+
+  outputs = { self, nixpkgs }: {
+
+    packages.x86_64-linux.monitoring-backpack = let pkgs = import nixpkgs { system = "x86_64-linux"; }; in pkgs.stdenv.mkDerivation rec {
+      name = "monitoring-backpack";
+      src = pkgs.fetchFromGitHub {
+        owner = "pkern";
+        repo = "monitoring-backpack";
+        rev = "539e1a92737274513f6ee7bb5648ba6d85f9220e";
+        sha256 = "1vs50r1gcif9x9nmn31qdqzz2z9d7psvrfsh4lqzxnqxnf5f4bwq";
+      };
+      installPhase = ''
+        mkdir -p $out/rules.d/
+        cp rules.d/* $out/rules.d/
+      '';
+    };
+
+    defaultPackage.x86_64-linux = self.packages.x86_64-linux.monitoring-backpack;
+
+  };
+}
