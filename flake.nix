2c07de680a5d03efad2635404e9b27cb09e5776f
Philipp Kern <phil@philkern.de>
2024-03-19 22:38:32 +0100

    flake.nix: Check and test rules during Flake build.

--- flake.nix
+++ flake.nix
@@ -11,6 +11,12 @@
         rev = "539e1a92737274513f6ee7bb5648ba6d85f9220e";
         sha256 = "1vs50r1gcif9x9nmn31qdqzz2z9d7psvrfsh4lqzxnqxnf5f4bwq";
       };
+      buildInputs = [ pkgs.prometheus ];
+      doCheck = true;
+      checkPhase = ''
+        ${pkgs.prometheus.cli}/bin/promtool check rules rules.d/*.yml
+        ${pkgs.prometheus.cli}/bin/promtool test rules tests/*.yml
+      '';
       installPhase = ''
         mkdir -p $out/rules.d/
         cp rules.d/* $out/rules.d/
